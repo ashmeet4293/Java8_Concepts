38c3c174e3ef5936c36ecc17aca6dddf15f1c0cc
Pawan Tiwari <ptiwari@keawings.com>
2020-12-21 13:02:23 +0545

    Running public static void main using interface

---
+++ default_and_static_method_in_interface/MainMethodOnInterface.java
@@ -0,0 +1,8 @@
+package com.company.practice.default_and_static_method_in_interface;
+
+/*We can also run public static void main() method in interface using java 8 version*/
+public interface MainMethodOnInterface {
+    public static void main(String[] args) {
+        System.out.println("Hello World ! Iam from interface using java 8");
+    }
+}

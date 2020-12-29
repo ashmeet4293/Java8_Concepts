c40035bd9a6d366b4249ea89537da426d6a8c093
Pawan Tiwari <ptiwari@keawings.com>
2020-12-29 11:56:59 +0545

    Stream concepts

---
+++ streams/StreamProgram.java
@@ -0,0 +1,51 @@
+package com.company.practice.streams;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StreamProgram {
+
+    public static void main(String[] args) {
+
+        List<Student> studentsMock = Student.getStudents();
+
+        System.out.println("________________STUDENT HAVING PERCENTAGE LESS THAN 55_______________");
+        /*Filtering the students based on percentage<55*/
+        List<Student> studentList = studentsMock.stream().filter(student -> student.percentage<55).collect(Collectors.toList());
+        studentList.forEach(System.out::println);
+
+
+        System.out.println("________________INCREASE PERCENT BY 1_______________");
+        /*Increasing the students percentage by 1*/
+        List<Student> increasedStudentPercentage = studentsMock.stream().map(student -> {
+            student.percentage=student.percentage+1;
+            return student;
+        }).collect(Collectors.toList());
+        increasedStudentPercentage.forEach(System.out::println);
+
+        System.out.println("________________Total Number of students WHOSE PERCENTAGE IS LESS THAN 55_______________");
+        long totalStudents = studentList.stream().count();
+        System.out.println(totalStudents);
+
+        System.out.println("________________LIST OF STUDENT SORTED BASED ON PERCENTAGE DESC_______________");
+        List<Student> sortedStudents= studentsMock.stream().sorted((student1, student2) -> student1.percentage < student2.percentage ? 1 : -1).collect(Collectors.toList());
+        sortedStudents.forEach(System.out::println);
+
+        System.out.println("________________LOWEST PERCENTAGE STUDENT_______________");
+       Student lowestPercentageStudent= studentsMock.stream().min((student1, student2) -> student1.percentage > student2.percentage ? 1 : -1).get();
+        System.out.println(lowestPercentageStudent);
+
+        System.out.println("________________IS STUDENT PERCENTAGE REPRESENT FAILED_______________");
+        boolean isFailedStudent=studentsMock.stream().noneMatch(student -> student.percentage < 40);
+        Predicate<Student> passedWithDistinction = student -> student.percentage >= 80;
+        boolean isSomeStudentWithDistinction =studentsMock.stream().anyMatch(passedWithDistinction);
+        boolean isAllStudentWithDistinction = studentsMock.stream().allMatch(passedWithDistinction);
+
+        System.out.println("DO WE HAVE FAILED STUDENT ? "+isFailedStudent);
+        System.out.println("DO WE HAVE SOME STUDENTS WITH DISTINCTION ? "+isSomeStudentWithDistinction);
+        System.out.println("DO WE HAVE ALL STUDENTS WITH DISTINCTION  ? "+isAllStudentWithDistinction);
+
+
+    }
+}
---
+++ streams/Student.java
@@ -0,0 +1,52 @@
+package com.company.practice.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student{
+    String name;
+    double percentage;
+
+    public Student(String name, double percentage){
+        this.name=name;
+        this.percentage=percentage;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", percentage =" + percentage +
+                '}';
+    }
+
+    public static List<Student> getStudents(){
+        List<Student> students= new ArrayList<>();
+        students.add(new Student("Ashmeet", 20));
+        students.add(new Student("Tiwari", 69));
+        students.add(new Student("Pawan", 65));
+        students.add(new Student("Chinkiy", 60));
+        students.add(new Student("Pinky", 80));
+        students.add(new Student("Reeta", 100));
+        students.add(new Student("Viraj", 54));
+
+        return students;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+}
